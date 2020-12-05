0aaf35fcbcd9e0f354f125d10576d5c0faf345d5
tsumotaku3 <75417882+tsumotaku3@users.noreply.github.com>
2020-12-05 10:43:26 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Test : MonoBehaviour
+{
+    //int型の配列「array」を初期化
+    int[] array = {1,2,3,4,5};
+
+    void Start()
+    {
+        //配列の要素をすべて表示する
+        for (int i=0;i<array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        //配列の要素をすべて表示する
+        for (int i=0;i<array.Length;i++)
+        {
+            Debug.Log(array[array.Length-i-1]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
